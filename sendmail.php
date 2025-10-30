ef6e691a0f3a72e6b249d1603ed6853d15da307e
MuhammadNabeel <nabeelm726@gmail.com>
2025-10-30 15:28:53 +0500

    mail

--- sendmail.php
+++ sendmail.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $mobile = htmlspecialchars($_POST['mobile']);
